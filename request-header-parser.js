697072019dd77bf02fa7878cf63b462b2660eca5
Zack Ward <zack@zadiastudio.com>
2017-01-10 15:19:17 -0700

    Check to see if we're behind a proxy, and use the correct IP

--- request-header-parser.js
+++ request-header-parser.js
@@ -6,8 +6,12 @@ var port = 3001;
 app.use(helmet());
 
 app.get('/api/whoami', function (req, res) {
+    
+    // App will be deployed behind an Nginx reverse proxy, so we need this to get the original IP address if we're in production
+    var remoteIP = req.get('X-Real-IP') == null ? req.ip : req.get('X-Real-IP');
+    
     res.json({
-        ipaddress: req.ip,
+        ipaddress: remoteIP,
         language: req.get('Accept-Language'),
         software: req.get('User-Agent')
     });
